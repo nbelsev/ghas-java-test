2586e551fd727c145aa32f4124c38070658ae112
nbelsev <n.bloor@elsevier.com>
2025-09-02 16:05:43 +0100

    Update to make XXE exploitable based on external input.

---
+++ src/main/java/nbelsev/test/xxe/XXEController.java
@@ -0,0 +1,30 @@
+package nbelsev.test.xxe;
+
+import java.sql.*;
+
+import java.sql.Connection;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class XXEController {
+	private static Connection connection;
+	
+	@GetMapping("/")
+	@ResponseBody
+	public String testXml(@RequestParam(name = "xml", required = false) String xmlPath,
+							   @RequestParam(name = "xsd", required = false) String xsdPath) {
+		String out;
+		
+		if(XXEDemo.validateXml(xmlPath, xsdPath)) {
+			out = "Validation result: success!";
+		} else {
+			out = "Validation result: failed.";
+		}
+		
+		return out;
+	}
+}
